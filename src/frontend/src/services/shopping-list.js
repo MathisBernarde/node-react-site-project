d30c11ef771c8e91afd51e749c180254f8f55614
doteur <zlatheomovich@gmail.com>
2025-12-11 19:05:13 +0100

    ajout readme

--- src/frontend/src/services/shopping-list.js
+++ src/frontend/src/services/shopping-list.js
@@ -2,11 +2,13 @@ import api from "./api";
 const SERVICE_URL = "/shopping-lists";
 
 export default {
+  // Récupérer toutes les listes
     getAll: async () => {
     const response = await api.get(SERVICE_URL);
     return response.data;
   },
 
+  // Créer une liste
   create: async (title) => {
     const response = await api.post(SERVICE_URL, { 
         title, 
@@ -14,10 +16,14 @@ export default {
     });
     return response.data;
   },
+
+  // Mettre à jour (ex: changer le statut)
   update: async (id, data) => {
     const response = await api.patch(`${SERVICE_URL}/${id}`, data);
     return response.data;
   },
+
+  // Supprimer
   delete: async (id) => {
     await api.delete(`${SERVICE_URL}/${id}`);
   }
